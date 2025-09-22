6b1c24a8283c905f0db1c96ca8f693f07997dd9c
Frank Smith <73555034+FrankSmith685@users.noreply.github.com>
2025-09-22 14:32:08 -0500

    update mm

--- src/app/contactanos/page.tsx
+++ src/app/contactanos/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Breadcrumbs from "@/components/contactanos/BreadCrumb";
-import ContactanosOverview from "@/components/contactanos/ContactanosOverview";
+// import ContactanosOverview from "@/components/contactanos/ContactanosOverview";
 import Header from "@/components/contactanos/header";
 
 export default function Contactanos() {
@@ -9,7 +9,7 @@ export default function Contactanos() {
         <div className="w-full">
             <Header/>
             <Breadcrumbs/>
-            <ContactanosOverview/>
+            {/* <ContactanosOverview/> */}
         </div>
     );
 }
